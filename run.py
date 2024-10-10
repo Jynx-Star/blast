1a91f5e66e9b1220c3f5b3856ac9d13773c32f99
JYNX-EXE <rustapchaudhary021@gmail.com>
2024-10-10 16:12:42 +0545

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+import os, platform, time, sys
+os.system('xdg-open https://t.me/jynxhub')
+try:
+ import requests
+except:os.system("pip install requests")
+print('\u001b[37m \x1b[38;5;196m->\033[1;97mChecking For Update...')
+os.system('git pull --quiet ')
+bit = platform.architecture()[0]
+if bit == "64bit":
+ print('\u001b[37m \x1b[38;5;196m->\033[1;97m64Bit Found')
+ time.sleep(2)
+ from file import ruspt
+ ruspt()
+ 
+ 
+ 
+if bit == "32bit":
+ print('\u001b[37m \x1b[38;5;196m-> \033[1;97m32Bit Found')
+ time.sleep(2)
+ from file import ruspt
+ ruspt()
